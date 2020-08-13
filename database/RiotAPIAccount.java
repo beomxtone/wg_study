8df1a97f907ee95da0dea44325828da320422111
yjs05011 <68629872+yjs05011@users.noreply.github.com>
2020-08-13 14:37:33 +0900

    accountid추출방법 궁리중

--- database/RiotAPIAccount.java
+++ database/RiotAPIAccount.java
@@ -5,20 +5,47 @@ import java.util.*;
 
 
 public class RiotAPIAccount {
-    
-    public static void main(String[] arg) throws MalformedURLException, IOException {
-        URL url = new URL("https://kr.api.riotgames.com/lol/league/v4/masterleagues/by-queue/RANKED_SOLO_5x5?api_key=RGAPI-94b8ac4d-5d9c-4cc4-ac37-ab8e69176465");
+    public static List<String> leagueTier(String tier) throws IOException {
+        int page = 1;
+        String info;
+        List <String> json = new ArrayList<String>();
+        URL url = new URL("https://kr.api.riotgames.com/lol/league/v4/entries/RANKED_SOLO_5x5/",tier, page, "&api_key=RGAPI-8abce829-705a-467a-83c0-d39923c30ac7");
         InputStream openStream = url.openStream();
         InputStreamReader Summoner = new InputStreamReader(openStream, "UTF-8");
         BufferedReader read = new BufferedReader(Summoner);
         while (true) {
-            String info = read.readLine();
-            if(info == null){
+            info = read.readLine();
+            if (info == null) {
                 break;
             }
-            System.out.println(info);
+            json.add(info);
+             
         }
         read.close();
         Summoner.close();
+        return json;
+    }
+
+    public static List<String> leagueTier(URL tier) throws IOException {
+        String info;
+        List <String> json = new ArrayList<String>();
+        URL url = new URL(tier, "?api_key=RGAPI-8abce829-705a-467a-83c0-d39923c30ac7");
+        InputStream openStream = url.openStream();
+        InputStreamReader Summoner = new InputStreamReader(openStream, "UTF-8");
+        BufferedReader read = new BufferedReader(Summoner);
+        while (true) {
+            info = read.readLine();
+            if (info == null) {
+                break;
+            }
+            json.add(info);
+        }
+        read.close();
+        Summoner.close();
+        return json;
+    }
+
+    public static void main(String[] args) throws IOException {
+        leagueTier("DIAMOND/I?page=");
     }
 }
